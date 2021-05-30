d4a5ecc66438c9cea3b36072439115606f30b580
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-05-30 13:24:58 +0930

    Store Setup and Feathers/Figbird setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import io from 'socket.io-client';
+import feathers from '@feathersjs/client';
+import { Provider, useFind } from 'figbird';
+import { StoreProvider } from 'easy-peasy';
+import store from './store/store';
 import App from './App';
 
+const socket = io('http://localhost:3030');
+const client = feathers();
+
+client.configure(feathers.socketio(socket));
+client.configure(
+  feathers.authentication({
+    storage: window.localStorage,
+  }),
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StoreProvider store={store}>
+    <React.StrictMode>
+      <Provider feathers={client}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </StoreProvider>,
   document.getElementById('root'),
 );
