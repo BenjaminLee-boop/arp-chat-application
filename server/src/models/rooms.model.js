0faef39b7af58a32642fe8f0d44e0a88cff9a96d
BenjaminLee-boop <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-05-30 13:47:17 +0930

    Server

---
+++ server/src/models/rooms.model.js
@@ -0,0 +1,26 @@
+// rooms-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const modelName = 'rooms';
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const schema = new Schema(
+    {
+      avatarUri: { type: String },
+      roomName: { type: String, required: true },
+      roomOwner: { type: String, required: true },
+    },
+    {
+      timestamps: true,
+    },
+  );
+
+  // This is necessary to avoid model compilation errors in watch mode
+  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+  if (mongooseClient.modelNames().includes(modelName)) {
+    mongooseClient.deleteModel(modelName);
+  }
+  return mongooseClient.model(modelName, schema);
+};
---
+++ server/src/services/users/users.hooks.js
@@ -0,0 +1,42 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+const { setField } = require('feathers-authentication-hooks');
+const { disallow } = require('feathers-hooks-common');
+
+const internalOnly = disallow('external');
+
+const limitToUser = setField({
+  from: 'params.user._id',
+  as: 'params.query._id',
+});
+
+module.exports = {
+  before: {
+    all: [],
+    find: [authenticate('jwt'), limitToUser],
+    get: [authenticate('jwt'), limitToUser],
+    create: [internalOnly],
+    update: [internalOnly],
+    patch: [internalOnly],
+    remove: [authenticate('jwt'), limitToUser],
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
+};
